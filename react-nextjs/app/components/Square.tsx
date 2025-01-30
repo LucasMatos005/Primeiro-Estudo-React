7b113de6ee3e25f5b765cf07001d5d6c363293dc
LucasMatos005 <lucas.amatos@sptech.school>
2025-01-29 23:53:04 -0300

    primeiros compontentes dois

---
+++ react-nextjs/app/components/Square.tsx
@@ -0,0 +1,19 @@
+export const Square = () => {
+    return(
+        <div className="w-40 h-40 bg-red-600 ">
+            Quadrado
+        </div>
+    );
+}
+
+const Circle = () => {
+    return(
+        <div className="w-40 h-40 bg-blue-600 rounded-full">
+            Círculo
+        </div>
+    );
+}
+
+export default Circle;
+
+//Export Named, Named export, exportação nomeada
---
+++ react-nextjs/app/page2.tsx
@@ -0,0 +1,11 @@
+//Funcional Components 
+function Page() {
+ return(
+    <div>
+       <h1>Componente 2</h1>
+       <p>Algum texto componente 2</p>
+    </div>
+    );
+}
+
+export default Page;
